caf94fef13da5722cf19af7ea8efe209a14e7a58
paolalenti <kirill251104@gmail.com>
2023-12-17 11:16:00 +0300

    last2023

--- env/Scripts/form_project/form_example/views.py
+++ env/Scripts/form_project/form_example/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
-# Create your views here.
+
 def form_example(request):
-    return render(request, "form-example.html")
+    form = ExampleForm()
+    for name in request.POST:
+        print("{}: {}".format(name, request.POST.getlist(name)))
+
+    return render(
+        request, "form-example.html", {"method": request.method, "form": form}
+    )
